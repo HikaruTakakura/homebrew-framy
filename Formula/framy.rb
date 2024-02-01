086299cb4d3b9e2951c06f1e47d3620054213303
HikaruTakakura <takakurahikaru@gmail.com>
2024-02-02 00:15:07 +0900

    Update framy.rb

--- Formula/framy.rb
+++ Formula/framy.rb
@@ -1,8 +1,8 @@
 class Framy < Formula
   desc "cli enframes images written in Rust"
   homepage "https://github.com/HikaruTakakura/framy"
-  url "https://github.com/HikaruTakakura/framy/releases/download/v0.1.1/framy-mac.tar.gz"
-  sha256 "8cee6e75609b25679dd0b659ae44df5d7e3485cc53388ab0f1d5e92abe0fbcdf"
+  url "https://github.com/HikaruTakakura/framy/releases/download/v0.1.1/framy-for-mac.tar.gz"
+  sha256 "5daf78f47e6041e974ed52203038b8d2e4d93e9f728c1c4a79d02ffe0feb0ef6"
   version "0.1.1"
 
   def install
